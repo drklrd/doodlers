3943ad464ea080a5a71737e9e117394aef9d68f8
saurav <saurav@zyoba.com>
2016-01-16 17:26:59 +0545

    adds missing sample-config.js file

---
+++ sample-config.js
@@ -0,0 +1,31 @@
+// module.exports = {
+
+// 	mysql : {
+// 		username : 'root',
+// 		password : '',
+// 		database : 'doodlers',
+// 		host : '127.0.0.1',
+// 		dialect : 'mysql',
+// 		port : 3306
+
+// 	},
+
+// 	jwtSecret : 'shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh!'
+// }
+
+
+
+module.exports = {
+
+	mysql : {
+		user : 'root',
+		password : '',
+		database : 'doodlers',
+		host : '127.0.0.1',
+		dialect : 'mysql',
+		port : 3306
+
+	},
+
+	jwtSecret : 'shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh!'
+}
